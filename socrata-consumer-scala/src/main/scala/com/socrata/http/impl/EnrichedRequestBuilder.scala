8d1d16e450f0a3e7e5b7dfaf9a77fdbb00d5966f
Robert J. Macomber <robert.macomber@socrata.com>
2012-07-24 20:26:50 -0700

    Add scala-friendly version of setQueryParameters to request builder

--- socrata-consumer-scala/src/main/scala/com/socrata/http/impl/EnrichedRequestBuilder.scala
+++ socrata-consumer-scala/src/main/scala/com/socrata/http/impl/EnrichedRequestBuilder.scala
@@ -1,6 +1,8 @@
 package com.socrata.http.impl
 
-import com.ning.http.client.{RequestBuilderBase, Realm}
+import scala.collection.JavaConverters._
+
+import com.ning.http.client.{FluentStringsMap, Realm, RequestBuilderBase}
 import com.ning.http.client.Realm.RealmBuilder
 
 import com.socrata.http.{Authorization, NoAuth, BasicAuth}
@@ -13,4 +15,7 @@ class EnrichedRequestBuilder[T <: RequestBuilderBase[T]](b: T) {
       case BasicAuth(username, pwd, appToken) =>
         b.setRealm(new RealmBuilder().setScheme(Realm.AuthScheme.BASIC).setPrincipal(username).setPassword(pwd).build).addHeader("X-App-Token", appToken)
     }
+
+  def setQueryParametersS(params: Map[String, Seq[String]]) =
+    b.setQueryParameters(new FluentStringsMap(params.mapValues(_.asJavaCollection).asJava))
 }
