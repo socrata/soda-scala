36291e771c35c74d1a1278cf22e4c139c71cec10
Robert J. Macomber <robert.macomber@socrata.com>
2012-08-06 10:27:24 -0700

    Document LowLevel and add executeJson method

--- socrata-consumer-scala/src/main/scala/com/socrata/soda2/consumer/LowLevel.scala
+++ socrata-consumer-scala/src/main/scala/com/socrata/soda2/consumer/LowLevel.scala
@@ -2,20 +2,40 @@ package com.socrata.soda2.consumer
 
 import java.net.URI
 
+import com.rojoma.json.ast.JValue
+
 import com.socrata.soda2.consumer.impl.QueryDisambiguator
 import com.socrata.future.Future
 import com.socrata.iteratee.CharIteratee
 import com.socrata.soda2.{Resource, Soda2Metadata}
 
+// Should this be moved to the soda2 package?  It's not a consumer-specific thing...
+
 /** Very low-level access to a SODA2 server.  This encapsulates all HTTP actions so higher-level code
- * such as a [[com.socrata.soda2.consumer.Simple]] object can deal purely with the data returned. */
+ * such as a [[com.socrata.soda2.consumer.Consumer]] object can deal purely with the data returned. */
 trait LowLevel {
   /** Executes a GET query against a SODA2 server and feeds the character data returned into the given
-   * [[com.socrata.iteratee.Iteratee]]. */
+   * [[com.socrata.iteratee.Iteratee]].
+   *
+   * @param resource The resource to retrieve.
+   * @param getParameters The values to feed into query.
+   * @param iteratee A handler for the response. */
   def execute[T](resource: Resource, getParameters: Map[String, Seq[String]], iteratee: (URI, Soda2Metadata) => CharIteratee[T]): Future[T]
 
   /** Executes a GET query against a SODA2 server and feeds the character data returned into the given
-   * [[com.socrata.iteratee.Iteratee]]. */
+   * [[com.socrata.iteratee.Iteratee]].
+   *
+   * @param resource The resource to retrieve.
+   * @param getParameters The values to feed into query.
+   * @param iteratee A handler for the response. */
   def execute[T](resource: Resource, getParameters: Map[String, String], iteratee: (URI, Soda2Metadata) => CharIteratee[T])(implicit stupidErasure: QueryDisambiguator): Future[T] =
     execute(resource, getParameters.mapValues(Seq(_)), iteratee)
+
+  /** Executes a POST query against a SODA2 server and feeds the character data returned into the given
+   * [[com.socrata.iteratee.Iteratee]].
+   *
+   * @param resource The resource to which to send JSON.
+   * @param jvalue The JSON to serialize into the request.
+   * @param iteratee A handler for the response. */
+  def executeJson[T](resource: Resource, jvalue: JValue, iteratee: (URI, Soda2Metadata) => CharIteratee[T]): Future[T]
 }
--- socrata-consumer-scala/src/main/scala/com/socrata/soda2/consumer/http/LowLevelHttp.scala
+++ socrata-consumer-scala/src/main/scala/com/socrata/soda2/consumer/http/LowLevelHttp.scala
@@ -6,6 +6,8 @@ import scala.io.Codec
 import java.net.{URI, URL}
 
 import com.ning.http.client.AsyncHttpClient
+import com.rojoma.json.ast.JValue
+import com.rojoma.json.io.CompactJsonWriter
 
 import com.socrata.soda2.consumer.LowLevel
 import com.socrata.soda2.consumer.impl.ResultProducer
@@ -16,22 +18,42 @@ import com.socrata.http.implicits._
 import com.socrata.iteratee.CharIteratee
 import com.socrata.soda2.{Resource, Soda2Metadata}
 
-// should this be moved to soda2.http?
+// should this be moved to soda2.http?  See similar comment on LowLevel.
 class LowLevelHttp(val client: AsyncHttpClient, val host: String, val port: Int, val authorization: Authorization)(implicit executionContext: ExecutionContext) extends LowLevel {
   import LowLevelHttp._
 
   def execute[T](resource: Resource, getParameters: Map[String, Seq[String]], iteratee: (URI, Soda2Metadata) => CharIteratee[T]): Future[T] =
-    doGet(uriForResource(resource), resource, Some(getParameters), iteratee).flatMap(maybeRetry(resource, getParameters, iteratee, _))
+    execute(uriForResource(resource), resource, getParameters, iteratee)
 
-  def doGet[T](uri: URI, resource: Resource, queryParameters: Option[Map[String, Seq[String]]], iteratee: (URI, Soda2Metadata) => CharIteratee[T]): Future[Retryable[T]] = {
-    val builder = client.prepareGet(uri.toString).
+  def execute[T](uri: URI, originalResource: Resource, getParameters: Map[String, Seq[String]], iteratee: (URI, Soda2Metadata) => CharIteratee[T]): Future[T] =
+    doGet(uri, originalResource, Some(getParameters), iteratee).flatMap(maybeRetry(uri, originalResource, getParameters, iteratee, _))
+
+  def executeJson[T](resource: Resource, body: JValue, iteratee: (URI, Soda2Metadata) => CharIteratee[T]): Future[T] =
+    executeJson(uriForResource(resource), resource, body, iteratee)
+
+  def executeJson[T](uri: URI, originalResource: Resource, body: JValue, iteratee: (URI, Soda2Metadata) => CharIteratee[T]): Future[T] =
+    doPost(uri, originalResource, body, iteratee).flatMap(maybeRetryJson(uri, originalResource, body, iteratee, _))
+
+  def doGet[T](uri: URI, originalResource: Resource, queryParameters: Option[Map[String, Seq[String]]], iteratee: (URI, Soda2Metadata) => CharIteratee[T]): Future[Retryable[T]] = {
+    val builder = client.prepareGet(uri.toURL.toString).
       maybeSetQueryParametersS(queryParameters).
       setFollowRedirects(true).
       setHeader("Accept", "application/json").
       authorize(authorization)
     log.debug("Making request to {}", uri)
     queryParameters.foreach{ p => log.debug("With query parameters {}", com.rojoma.json.util.JsonUtil.renderJson(p)) }
-    builder.makeRequest(new StandardConsumer(resource, bodyConsumer(_, _, iteratee(uri, _))))
+    builder.makeRequest(new StandardConsumer(originalResource, bodyConsumer(_, _, iteratee(uri, _))))
+  }
+
+  def doPost[T](uri: URI, originalResource: Resource, body: JValue, iteratee: (URI, Soda2Metadata) => CharIteratee[T]): Future[Retryable[T]] = {
+    val builder = client.preparePost(uri.toString).
+      setFollowRedirects(true).
+      setHeader("Accept", "application/json").
+      setHeader("Content-type", "application/json; charset=utf-8").
+      authorize(authorization).
+      setBody(CompactJsonWriter.toString(body).getBytes("utf-8"))
+    log.debug("Making request to {}", uri)
+    builder.makeRequest(new StandardConsumer(originalResource, bodyConsumer(_, _, iteratee(uri, _))))
   }
 
   def bodyConsumer[T](headers: Headers, codec: Codec, iteratee: Soda2Metadata => CharIteratee[T]) = {
@@ -45,10 +67,14 @@ class LowLevelHttp(val client: AsyncHttpClient, val host: String, val port: Int,
     new ResultProducer(codec, iteratee(soda2Metadata.asScala.mapValues(_.get(0))))
   }
 
-  def uriForResource(resource: Resource) =
-    new URL("https", host, port, "/id/" + resource.toString).toURI
+  val protocol =
+    if(java.lang.Boolean.getBoolean("com.socrata.soda2.forceHttp")) "http"
+    else "https"
 
-  def maybeRetry[T](resource: Resource, getParameters: Map[String, Seq[String]], iteratee: (URI, Soda2Metadata) => CharIteratee[T], x: Retryable[T]): Future[T] = x match {
+  def uriForResource(resource: Resource) =
+    new URL(protocol, host, port, "/id/" + resource.toString).toURI
+
+  def maybeRetry[T](uri: URI, originalResource: Resource, getParameters: Map[String, Seq[String]], iteratee: (URI, Soda2Metadata) => CharIteratee[T], x: Retryable[T]): Future[T] = x match {
     case Right(result) =>
       Future.now(result)
     case Left(newRequest) =>
@@ -57,12 +83,31 @@ class LowLevelHttp(val client: AsyncHttpClient, val host: String, val port: Int,
       executionContext.in(newRequest.retryAfter) {
         newRequest match {
           case Retry(_, _) =>
-            execute(resource, getParameters, iteratee)
+            execute(uri, originalResource, getParameters, iteratee)
           case RetryWithTicket(ticket, _, _) =>
-            execute(resource, getParameters + ("ticket" -> Seq(ticket)), iteratee)
+            execute(uri, originalResource, getParameters + ("ticket" -> Seq(ticket)), iteratee)
           case Redirect(url, _, _) =>
-            val target = uriForResource(resource).resolve(url)
-            doGet(target, resource, None, iteratee).flatMap(maybeRetry(resource, getParameters, iteratee, _))
+            val target = uri.resolve(url)
+            doGet(target, originalResource, None, iteratee).flatMap(maybeRetry(target, originalResource, getParameters, iteratee, _))
+        }
+      }.flatten
+  }
+
+  def maybeRetryJson[T](uri: URI, originalResource: Resource, body: JValue, iteratee: (URI, Soda2Metadata) => CharIteratee[T], x: Retryable[T]): Future[T] = x match {
+    case Right(result) =>
+      Future.now(result)
+    case Left(newRequest) =>
+      // TODO: some sort of progress based on newRequest.details
+      log.debug("Got 202; retrying in {}s", newRequest.retryAfter)
+      executionContext.in(newRequest.retryAfter) {
+        newRequest match {
+          case Retry(_, _) =>
+            executeJson(uri, originalResource, body, iteratee)
+          case RetryWithTicket(ticket, _, _) =>
+            execute(uri, originalResource, Map("ticket" -> Seq(ticket)), iteratee)
+          case Redirect(url, _, _) =>
+            val target = uri.resolve(url)
+            doGet(target, originalResource, None, iteratee).flatMap(maybeRetry(target, originalResource, Map.empty, iteratee, _))
         }
       }.flatten
   }
