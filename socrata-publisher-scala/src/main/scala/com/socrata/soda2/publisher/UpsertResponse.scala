547ed7f218935474139ae3d7710c193e231bd6fd
Robert J. Macomber <robert.macomber@socrata.com>
2012-08-12 06:42:53 -0700

    Full upsert response

--- socrata-publisher-scala/src/main/scala/com/socrata/soda2/publisher/UpsertResponse.scala
+++ socrata-publisher-scala/src/main/scala/com/socrata/soda2/publisher/UpsertResponse.scala
@@ -2,8 +2,14 @@ package com.socrata.soda2.publisher
 
 import com.rojoma.json.util.SimpleJsonCodecBuilder
 
-case class UpsertResponse(created: Int)
+case class UpsertResponse(created: Int, updated: Int, byIdentifier: Int, deleted: Int, errors: Int, bySid: Int)
 
 object UpsertResponse {
-  implicit val jCodec = SimpleJsonCodecBuilder[UpsertResponse].gen("Rows Created", _.created)
+  implicit val jCodec = SimpleJsonCodecBuilder[UpsertResponse].gen(
+    "Rows Created", _.created,
+    "Rows Updated", _.updated,
+    "By RowIdentifier", _.byIdentifier,
+    "Rows Deleted", _.deleted,
+    "Errors", _.errors,
+    "By SID", _.bySid)
 }
