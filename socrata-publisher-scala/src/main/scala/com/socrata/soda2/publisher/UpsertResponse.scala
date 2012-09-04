cdb7de36d7fdf80f2178087156d5f4ba5e68091f
Robert J. Macomber <robert.macomber@socrata.com>
2012-09-04 16:11:18 -0700

    Upsert response is changing shape

--- socrata-publisher-scala/src/main/scala/com/socrata/soda2/publisher/UpsertResponse.scala
+++ socrata-publisher-scala/src/main/scala/com/socrata/soda2/publisher/UpsertResponse.scala
@@ -1,15 +1,31 @@
 package com.socrata.soda2.publisher
 
-import com.rojoma.json.util.SimpleJsonCodecBuilder
+import com.rojoma.json.util.{SimpleJsonCodecBuilder, SimpleHierarchyCodecBuilder, NoTag}
 
-case class UpsertResponse(created: Int, updated: Int, byIdentifier: Int, deleted: Int, errors: Int, bySid: Int)
+sealed abstract class UpsertResponse {
+  def created: Int
+  def updated: Int
+  def deleted: Int
+}
+case class UpsertResponseLegacy(created: Int, updated: Int, deleted: Int, byIdentifier: Int, errors: Int, bySid: Int) extends UpsertResponse
+case class UpsertResponse2(created: Int, updated: Int, deleted: Int) extends UpsertResponse
 
 object UpsertResponse {
-  implicit val jCodec = SimpleJsonCodecBuilder[UpsertResponse].build(
+  implicit val jCodecLegacy = SimpleJsonCodecBuilder[UpsertResponseLegacy].build(
     "Rows Created", _.created,
     "Rows Updated", _.updated,
-    "By RowIdentifier", _.byIdentifier,
     "Rows Deleted", _.deleted,
+    "By RowIdentifier", _.byIdentifier,
     "Errors", _.errors,
     "By SID", _.bySid)
+
+  implicit val jCodec2 = SimpleJsonCodecBuilder[UpsertResponse2].build(
+    "rows_created", _.created,
+    "rows_updated", _.updated,
+    "rows_deleted", _.deleted)
+
+  implicit val jCodec = SimpleHierarchyCodecBuilder[UpsertResponse](NoTag).
+    branch[UpsertResponseLegacy].
+    branch[UpsertResponse2].
+    build
 }
