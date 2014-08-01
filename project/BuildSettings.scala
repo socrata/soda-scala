0e5891fb6f9b2c8d5ecf1878c6b55c805f3194e6
John V Kew II <john.kew@socrata.com>
2014-07-31 20:29:30 -0700

    Remove all the cloudbees publish stuff so folks can build this externally.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -1,44 +1,18 @@
 import sbt._
 import Keys._
 
-import com.socrata.cloudbeessbt.SocrataCloudbeesSbt
-
 import Dependencies._
 
 object BuildSettings {
   val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++
-    SocrataCloudbeesSbt.socrataBuildSettings ++
     Seq(
       version := "2.0.0-SNAPSHOT",
       scalaVersion := "2.10.1",
-      crossScalaVersions := Seq("2.9.3", "2.10.1"),
-      // random stuff Sonatype wants
-      pomExtra := (
-        <url>http://www.github.com/socrata/soda-scala</url>
-        <licenses>
-          <license>
-            <name>The MIT License (MIT)</name>
-            <url>http://opensource.org/licenses/mit-license.php</url>
-            <distribution>repo</distribution>
-          </license>
-        </licenses>
-        <scm>
-          <url>git://github.com/socrata/soda-scala.git</url>
-          <connection>scm:git://github.com/socrata/soda-scala.git</connection>
-        </scm>
-        <developers>
-          <developer>
-            <name>Robert Macomber</name>
-            <email>robert.macomber@socrata.com</email>
-            <organization>Socrata</organization>
-          </developer>
-        </developers>
-      )
+      crossScalaVersions := Seq("2.9.3", "2.10.1")
     )
 
   def commonProjectSettings(assembly: Boolean = false): Seq[Setting[_]] =
     buildSettings ++
-    SocrataCloudbeesSbt.socrataProjectSettings(assembly) ++
     Seq(
       testOptions in Test ++= Seq(
         Tests.Argument(TestFrameworks.ScalaTest, "-oD")
--- project/build.sbt
+++ project/build.sbt
@@ -1,10 +1,8 @@
 resolvers ++= Seq(
-  "socrata releases" at "http://repository-socrata-oss.forge.cloudbees.com/release",
-  "DiversIT repo" at "http://repository-diversit.forge.cloudbees.com/release"
+  "socrata releases" at "http://repository-socrata-oss.forge.cloudbees.com/release"
 )
-
-addSbtPlugin("com.socrata" % "socrata-cloudbees-sbt" % "1.0.0")
 
 // resolvers += Resolver.url("artifactory", url("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
 
 // addSbtPlugin("com.jsuereth" % "xsbt-gpg-plugin" % "0.6")
+
